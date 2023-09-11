25e02eca2fec4f016f2110c5e8f61249fe174a5e
Helbushek <gorevaya134@gmail.com>
2023-09-11 14:33:44 +0700

    Array Shake function

--- AADS/SalesmanProblem/salesman.h
+++ AADS/SalesmanProblem/salesman.h
@@ -1,16 +1,46 @@
 #pragma once
 #include <iostream>
 
-void swapInt(int* first, int*second) {
+void swapInt(int* first, int*second) 
+{
 	int temp = *first;
 	*first = *second;
 	*second = temp;
-
 }
 
-void shakeArray() {
-	// check git working 
+int shakeArray(int* array, int length) // generates next position for array, needed for finding every way possible (traveling between cities only once) 
+{ 
+	//1. Find location for 1st from right element that has bigger element right from him, wil be pos1
 
+	int* pos1, * pos2;
+
+	for (int* i = array + length-1, i != arary, i--) 
+	{
+		if (*i < *(i + 1)) {
+			pos1 = i;
+			break;
+		}
+	}
+
+	//2. Find location for 1st from right element that is bigger than the pos1, will be pos2, swap pos1 and pos2
+
+	for (int* j = array + length, j != array, j--)
+	{
+		if (*j > *pos1) {
+			pos2 = j;
+			break;
+		}
+	}
+
+	swapInt(pos2, pos1);
+
+	//4. Sort all the elements by increasing.
+
+	for (int* k = pos1 + 1 * l = array + length, k < l, k++, l--)
+	{
+		swap(k, l);
+	}
+	return(1);
 
 }
 
