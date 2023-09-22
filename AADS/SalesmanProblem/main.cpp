c53020556c708a6e5cc2970cf300d85f8ed48622
Helbushek <gorevaya134@gmail.com>
2023-09-22 11:21:26 +0700

    relocated functions from header to .cpp file

---
+++ AADS/SalesmanProblem/main.cpp
@@ -0,0 +1,194 @@
+#include "salesman.h"
+#include <iostream>
+#include <random>
+#include <time.h>
+#include <ctime>
+#include <fstream>
+
+void printArray(int* array, int length)
+{
+	for (int i = 0; i < length; i++) {
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void printDArray(int** doubleArray, int linesNumber, int columnsNumber)
+{
+	for (int i = 0; i < linesNumber; i++)
+	{
+		printArray(doubleArray[i], columnsNumber);
+		//std::cout << std::endl;
+	}
+}
+
+void generateArray(int* array, int length, int startOfRange, int endOfRange)
+{
+	
+
+	for (int i = 0; i < length; i++) {
+		array[i] = rand() % (endOfRange - startOfRange) + startOfRange;
+	}
+
+}
+
+void generateDoubleArray(int** doubleArray, int numberOfLines, int numberOfColumns, int startOfRange, int endOfRange)
+{
+	for (int i = 0; i < numberOfLines; i++)
+	{
+		generateArray(doubleArray[i], numberOfColumns, startOfRange, endOfRange);
+	}
+}
+
+void inputArray(int* array, int length)
+{
+	for (int i = 0; i < length; i++) {
+		std::cin >> array[i];
+	}
+
+}
+
+void inputDoubleArray(int** doubleArray, int numberOfLines, int numberOfColumns)
+{
+	for (int i = 0; i < numberOfLines; i++)
+	{
+		inputArray(doubleArray[i], numberOfColumns);
+	}
+}
+
+
+
+int main() {
+
+	srand(time(0));
+
+	// Work with user, input all variables
+	int length, startCity, numberOfRepeats;
+
+	std::cout << "\t\t\t\t Solving Salesman problem by enumeration" << std::endl << "Input variables: " << std::endl << std::endl << "Number of Cities" << std::endl << "City you want to srart from (Start City) " << std::endl << "Map of distances between cities";
+
+	std::cout << std::endl << std::endl << std::endl << "\t\t Do you want to input Road Price Map by hand or shall it be generated randomly?" << std::endl << "\t\t\t\t\t\tY/N" << std::endl << "\t\t\t\t\tBy Hand/Im too lazy, do it for me" << std::endl;
+
+	char choice;
+
+	std::cin >> choice;
+
+	std::cout << std::endl << "Please input Number of cities: ";
+	std::cin >> length;
+
+
+	std::cout << std::endl << "Please input number of city you want to start from: ";
+	std::cin >> startCity;
+
+	std::cout << std::endl << "Input number of repeats you want to run the programm: ";
+	std::cin >> numberOfRepeats;
+
+	std::ofstream statistic("statistic.txt");
+
+	if (statistic.is_open()) std::cout << std::endl << "File created";
+	else {
+		std::cout << std::endl << "File is not created"; return(-1);
+	}
+	statistic.width(6);
+
+	int summOfTime=0, summOfAccuracy = 0;
+
+	for (int i = 0; i < numberOfRepeats; i++) {
+
+		int** doubleArray = new int* [length];
+		for (int i = 0; i < length; i++)
+		{
+			doubleArray[i] = new int[length];
+		}
+
+		if (choice == 'N')
+		{
+			std::cout << std::endl << "Generating your price map" << std::endl;
+			generateDoubleArray(doubleArray, length, length, 1, 10); // doubleArray - map of distances between cities [i] - start city [j] - end city, is symmetrycal
+		}
+		if (choice == 'Y')
+		{
+			std::cout << std::endl << "Please input your price map: " << std::endl;
+			inputDoubleArray(doubleArray, length, length);
+		}
+
+		for (int i = 0; i < length; i++)
+		{
+			doubleArray[i][i] = 0;
+		}
+
+		std::cout << "Your final road map:" << std::endl;
+		printDArray(doubleArray, length, length);
+		std::cout << std::endl;
+
+
+
+		int* minRoadMap = new int[length + 1];
+		int* maxRoadMap = new int[length + 1];
+
+		unsigned int startClock, endClock, executionTime;
+
+		int bestDistance, worstDistance;
+
+		startClock = clock();
+		minRoadMap = solveSalesmanByEnumeration(doubleArray, length, startCity, bestDistance, worstDistance, maxRoadMap);
+		endClock = clock();
+		executionTime = endClock - startClock;
+
+		std::cout << "Min road map and distance: " << std::endl;
+		printArray(minRoadMap, length + 1);
+		std::cout << " summ: " << bestDistance << std::endl;
+
+		std::cout << std::endl << "Max road map and distance: " << std::endl;
+		printArray(maxRoadMap, length + 1);
+		std::cout << " summ: " << worstDistance << std::endl;
+
+		std::cout << std::endl << "Time of execution: " << (float)(executionTime / 1000) << " sec" << std::endl << std::endl;
+
+		statistic << "Enumeration method: " << std::endl << "Roadmap: "; 
+		for (int i = 0; i < length; i++) {
+			statistic << minRoadMap[i] << ' ';
+		}
+
+		statistic << std::endl << "Best distance " << bestDistance << std::endl << "Time of execution: " << (float)(executionTime / 1000) << std::endl;
+
+		int bestDistanceHeuristic = 0;
+		startClock = clock();
+		minRoadMap = solveSalesmanByHeuristics(doubleArray, length, startCity, &bestDistanceHeuristic);
+		endClock = clock();
+		int executionTimeHeuristic = endClock - startClock;
+
+		std::cout << std::endl << "Heuristics method solved problem: " << std::endl;
+		printArray(minRoadMap, length + 1);
+		std::cout << " summ: " << bestDistanceHeuristic << std::endl;
+		std::cout << std::endl << "Time of execution: " << (float)(executionTimeHeuristic / 1000) << " sec";
+
+		int percentTime;
+		if (executionTime != 0) percentTime = ((executionTime - executionTimeHeuristic) / executionTime) * 100;
+		else percentTime = 0;
+		std::cout << std::endl << "Hieristic is faster than enumeration by: " << percentTime << "%" << std::endl;
+		std::cout.precision(4);
+		std::cout << std::endl << "Hieristic`s accuracy is close to best by: " << (bestDistanceHeuristic - bestDistance) << " which is " << ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0 << "%" << std::endl;
+
+		statistic << "Heuristic method: " << std::endl << "Roadmap: ";
+		for (int i = 0; i < length; i++) {
+			statistic << minRoadMap[i] << ' ';
+		}
+
+		statistic << std::endl << "Best distance " << bestDistanceHeuristic << std::endl << "Time of execution: " << (float)(executionTimeHeuristic / 1000) << std::endl;
+		statistic << "Speed difference: " << percentTime << "%" << std::endl << "Precision difference: " << ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0 << "%" << std::endl << std::endl;
+		
+		summOfAccuracy += ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0;
+		summOfTime += percentTime;
+
+		delete minRoadMap;
+		delete maxRoadMap;
+		for (int i = 0; i < length; i++) {
+			delete doubleArray[i];
+		}
+		delete doubleArray;
+	}
+
+	statistic << std::endl <<"Arithmetic mean of time difference: " << summOfTime / numberOfRepeats << "%" << std::endl << "Arithmeric mean of prescision difference: " << summOfAccuracy / numberOfRepeats << "%";
+	return(0);
+}
--- AADS/SalesmanProblem/salesman.cpp
+++ AADS/SalesmanProblem/salesman.cpp
@@ -1,194 +1,153 @@
-#include "salesman.h"
 #include <iostream>
-#include <random>
-#include <time.h>
-#include <ctime>
-#include <fstream>
 
-void printArray(int* array, int length)
+void swapInt(int* first, int* second)
 {
-	for (int i = 0; i < length; i++) {
-		std::cout << array[i] << " ";
-	}
-	std::cout << std::endl;
+	int temp = *first;
+	*first = *second;
+	*second = temp;
 }
 
-void printDArray(int** doubleArray, int linesNumber, int columnsNumber)
+int shakeArray(int* array, int length) 
 {
-	for (int i = 0; i < linesNumber; i++)
+
+	int* pos1 = nullptr, * pos2 = nullptr;
+
+	for (int* i = array + length - 2; i >= array; i--)
 	{
-		printArray(doubleArray[i], columnsNumber);
-		//std::cout << std::endl;
-	}
-}
-
-void generateArray(int* array, int length, int startOfRange, int endOfRange)
-{
-	
-
-	for (int i = 0; i < length; i++) {
-		array[i] = rand() % (endOfRange - startOfRange) + startOfRange;
+		if (*i < *(i + 1)) {
+			pos1 = i;
+			break;
+		}
 	}
 
-}
+	if (pos1 == nullptr) return(0);
 
-void generateDoubleArray(int** doubleArray, int numberOfLines, int numberOfColumns, int startOfRange, int endOfRange)
-{
-	for (int i = 0; i < numberOfLines; i++)
+	for (int* j = array + length - 1; j >= array; j--)
 	{
-		generateArray(doubleArray[i], numberOfColumns, startOfRange, endOfRange);
-	}
-}
-
-void inputArray(int* array, int length)
-{
-	for (int i = 0; i < length; i++) {
-		std::cin >> array[i];
+		if (*j > *pos1) {
+			pos2 = j;
+			break;
+		}
 	}
 
-}
+	swapInt(pos2, pos1);
 
-void inputDoubleArray(int** doubleArray, int numberOfLines, int numberOfColumns)
-{
-	for (int i = 0; i < numberOfLines; i++)
+	for (int* k = pos1 + 1, *l = array + length - 1; k < l; k++, l--)
 	{
-		inputArray(doubleArray[i], numberOfColumns);
+		swapInt(k, l);
 	}
+
 }
 
+int* solveSalesmanByEnumeration(int** distanceMap, int numberOfCities, int startCity, int& minDistance, int& maxDistance, int* maxRoadMap) {
 
+	minDistance = INT_MAX;
+	maxDistance = 0;
+	int* minRoadMap = new int[numberOfCities + 1];
 
-int main() {
+	int* roadMap = new int[numberOfCities + 1];
 
-	srand(time(0));
-
-	// Work with user, input all variables
-	int length, startCity, numberOfRepeats;
-
-	std::cout << "\t\t\t\t Solving Salesman problem by enumeration" << std::endl << "Input variables: " << std::endl << std::endl << "Number of Cities" << std::endl << "City you want to srart from (Start City) " << std::endl << "Map of distances between cities";
-
-	std::cout << std::endl << std::endl << std::endl << "\t\t Do you want to input Road Price Map by hand or shall it be generated randomly?" << std::endl << "\t\t\t\t\t\tY/N" << std::endl << "\t\t\t\t\tBy Hand/Im too lazy, do it for me" << std::endl;
-
-	char choice;
-
-	std::cin >> choice;
-
-	std::cout << std::endl << "Please input Number of cities: ";
-	std::cin >> length;
-
-
-	std::cout << std::endl << "Please input number of city you want to start from: ";
-	std::cin >> startCity;
-
-	std::cout << std::endl << "Input number of repeats you want to run the programm: ";
-	std::cin >> numberOfRepeats;
-
-	std::ofstream statistic("statistic.txt");
-
-	if (statistic.is_open()) std::cout << std::endl << "File created";
-	else {
-		std::cout << std::endl << "File is not created"; return(-1);
-	}
-	statistic.width(6);
-
-	int summOfTime=0, summOfAccuracy = 0;
-
-	for (int i = 0; i < numberOfRepeats; i++) {
-
-		int** doubleArray = new int* [length];
-		for (int i = 0; i < length; i++)
-		{
-			doubleArray[i] = new int[length];
-		}
-
-		if (choice == 'N')
-		{
-			std::cout << std::endl << "Generating your price map" << std::endl;
-			generateDoubleArray(doubleArray, length, length, 1, 10); // doubleArray - map of distances between cities [i] - start city [j] - end city, is symmetrycal
-		}
-		if (choice == 'Y')
-		{
-			std::cout << std::endl << "Please input your price map: " << std::endl;
-			inputDoubleArray(doubleArray, length, length);
-		}
-
-		for (int i = 0; i < length; i++)
-		{
-			doubleArray[i][i] = 0;
-		}
-
-		std::cout << "Your final road map:" << std::endl;
-		printDArray(doubleArray, length, length);
-		std::cout << std::endl;
-
-
-
-		int* minRoadMap = new int[length + 1];
-		int* maxRoadMap = new int[length + 1];
-
-		unsigned int startClock, endClock, executionTime;
-
-		int bestDistance, worstDistance;
-
-		startClock = clock();
-		minRoadMap = solveSalesmanByEnumeration(doubleArray, length, startCity, bestDistance, worstDistance, maxRoadMap);
-		endClock = clock();
-		executionTime = endClock - startClock;
-
-		std::cout << "Min road map and distance: " << std::endl;
-		printArray(minRoadMap, length + 1);
-		std::cout << " summ: " << bestDistance << std::endl;
-
-		std::cout << std::endl << "Max road map and distance: " << std::endl;
-		printArray(maxRoadMap, length + 1);
-		std::cout << " summ: " << worstDistance << std::endl;
-
-		std::cout << std::endl << "Time of execution: " << (float)(executionTime / 1000) << " sec" << std::endl << std::endl;
-
-		statistic << "Enumeration method: " << std::endl << "Roadmap: "; 
-		for (int i = 0; i < length; i++) {
-			statistic << minRoadMap[i] << ' ';
-		}
-
-		statistic << std::endl << "Best distance " << bestDistance << std::endl << "Time of execution: " << (float)(executionTime / 1000) << std::endl;
-
-		int bestDistanceHeuristic = 0;
-		startClock = clock();
-		minRoadMap = solveSalesmanByHeuristics(doubleArray, length, startCity, &bestDistanceHeuristic);
-		endClock = clock();
-		int executionTimeHeuristic = endClock - startClock;
-
-		std::cout << std::endl << "Heuristics method solved problem: " << std::endl;
-		printArray(minRoadMap, length + 1);
-		std::cout << " summ: " << bestDistanceHeuristic << std::endl;
-		std::cout << std::endl << "Time of execution: " << (float)(executionTimeHeuristic / 1000) << " sec";
-
-		int percentTime;
-		if (executionTime != 0) percentTime = ((executionTime - executionTimeHeuristic) / executionTime) * 100;
-		else percentTime = 0;
-		std::cout << std::endl << "Hieristic is faster than enumeration by: " << percentTime << "%" << std::endl;
-		std::cout.precision(4);
-		std::cout << std::endl << "Hieristic`s accuracy is close to best by: " << (bestDistanceHeuristic - bestDistance) << " which is " << ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0 << "%" << std::endl;
-
-		statistic << "Heuristic method: " << std::endl << "Roadmap: ";
-		for (int i = 0; i < length; i++) {
-			statistic << minRoadMap[i] << ' ';
-		}
-
-		statistic << std::endl << "Best distance " << bestDistanceHeuristic << std::endl << "Time of execution: " << (float)(executionTimeHeuristic / 1000) << std::endl;
-		statistic << "Speed difference: " << percentTime << "%" << std::endl << "Precision difference: " << ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0 << "%" << std::endl << std::endl;
-		
-		summOfAccuracy += ((bestDistanceHeuristic * 1.0) - bestDistance) / (bestDistance * 1.0) * 100.0;
-		summOfTime += percentTime;
-
-		delete minRoadMap;
-		delete maxRoadMap;
-		for (int i = 0; i < length; i++) {
-			delete doubleArray[i];
-		}
-		delete doubleArray;
+	for (int i = 0; i < numberOfCities + 1; i++)
+	{
+		roadMap[i] = i + 1;
 	}
 
-	statistic << std::endl <<"Arithmetic mean of time difference: " << summOfTime / numberOfRepeats << "%" << std::endl << "Arithmeric mean of prescision difference: " << summOfAccuracy / numberOfRepeats << "%";
-	return(0);
+	int returned = 1;
+	roadMap[numberOfCities] = startCity;
+	while (roadMap[0] != startCity)
+	{
+		returned = shakeArray(roadMap, numberOfCities);
+	}
+
+	int currentDistance = 0;
+
+	while (returned != 0 && roadMap[0] == startCity)
+	{
+
+		for (int i = 0; i < numberOfCities; i++)
+		{
+			currentDistance += distanceMap[roadMap[i] - 1][roadMap[i + 1] - 1];
+		}
+
+		if (currentDistance < minDistance)
+		{
+			minDistance = currentDistance;
+			for (int i = 0; i < numberOfCities; i++)
+			{
+				minRoadMap[i] = roadMap[i];
+			}
+			minRoadMap[numberOfCities] = startCity;
+		}
+
+		if (currentDistance > maxDistance && maxRoadMap != nullptr)
+		{
+			maxDistance = currentDistance;
+			for (int i = 0; i < numberOfCities; i++)
+			{
+				maxRoadMap[i] = roadMap[i];
+			}
+			maxRoadMap[numberOfCities] = startCity;
+		}
+
+		currentDistance = 0;
+		returned = shakeArray(roadMap, numberOfCities);
+	}
+	delete roadMap;
+
+	return(minRoadMap);
+}
+
+int findMinInArray(int* array, int length) {
+	int min = INT_MAX, minIndex = -1;
+	for (int i = 0; i < length; i++) {
+		if (min > array[i] && array[i] != 0) {
+			minIndex = i;
+			min = array[i];
+		}
+	}
+	return(minIndex);
+}
+
+int* solveSalesmanByHeuristics(int** distanceMap, int numberOfCities, int startCity, int* minDistance)
+{
+	int* minRoadMap = new int[numberOfCities + 1];
+	//Choosing the outgoing arc of the minimum cost
+
+	int** copiedDistanceMap = new int* [numberOfCities];
+	for (int i = 0; i < numberOfCities; i++) {
+		copiedDistanceMap[i] = new int[numberOfCities];
+	}
+
+	for (int i = 0; i < numberOfCities; i++) {
+		for (int j = 0; j < numberOfCities; j++) {
+			copiedDistanceMap[i][j] = distanceMap[i][j];
+		}
+	}
+
+	int currentCity = startCity;
+	int temp = 0;
+	for (int j = 0; j < numberOfCities; j++) {
+		copiedDistanceMap[j][startCity - 1] = 0;
+	}
+	for (int i = 0; i < numberOfCities && temp != -1; i++) {
+		temp = findMinInArray(copiedDistanceMap[currentCity - 1], numberOfCities);
+		minRoadMap[i] = currentCity;
+		if (temp == -1) continue;
+
+		*minDistance += copiedDistanceMap[currentCity - 1][temp];
+		for (int j = 0; j < numberOfCities; j++) {
+			copiedDistanceMap[j][temp] = 0;
+			copiedDistanceMap[currentCity - 1][j] = 0;
+		}
+		copiedDistanceMap[temp][currentCity - 1] = 0;
+		currentCity = temp + 1;
+	}
+	minRoadMap[numberOfCities] = startCity;
+	*minDistance += distanceMap[(minRoadMap[numberOfCities - 1]) - 1][startCity - 1]; // 0<MinRoadMap[i]<=numberOfCities; 0<startCity<=numberOfCities; index: -1; 
+
+	for (int i = 0; i < numberOfCities; i++) {
+		delete[] copiedDistanceMap[i];
+	}
+	delete[] copiedDistanceMap;
+	return(minRoadMap);
 }
