0f4ba1cb1b3c0431dfc4d5c75852350ecd50645c
Helbushek <gorevaya134@gmail.com>
2023-09-29 10:43:30 +0700

    cosmetic features in bm_search main

--- AADS/BM search/main.cpp
+++ AADS/BM search/main.cpp
@@ -9,19 +9,19 @@ int main() {
 	char* stringToSearch = new char[4];
 	strcpy_s(stringToSearch, 4, "tor");
 	int* includes = new int[20];
-	std::cout << "(0,91)";
+	std::cout << "(0,91): ";
 	includes = searchByBmOnce(string, stringToSearch);
 
 	for (int i = 0; includes[i] != -1; i++) {
 		std::cout << includes[i] << ' ';
 	}
-	std::cout << std::endl << "(17,91)";
+	std::cout << std::endl << "(17,91): ";
 	includes = searchByBmOnce(string, stringToSearch, 17);
 
 	for (int i = 0; includes[i] != -1; i++) {
 		std::cout << includes[i] << ' ';
 	}
-	std::cout << std::endl << "(28, 36)";
+	std::cout << std::endl << "(28, 36): ";
 	includes = searchByBmOnce(string, stringToSearch, 28, 36);
 
 	for (int i = 0; includes[i] != -1; i++) {
