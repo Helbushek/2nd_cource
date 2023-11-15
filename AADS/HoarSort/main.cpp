e131d14a3832845bdb8096975acd20c1ee4ff993
Helbushek <gorevaya134@gmail.com>
2023-11-15 11:39:49 +0700

    copied cycles from earlier labs, found out that 1st hoar is not working for right recursions, fill be fixed

--- AADS/HoarSort/main.cpp
+++ AADS/HoarSort/main.cpp
@@ -1,12 +1,81 @@
 #include "HoarSort.h"
 #include <iostream>
+#include <string>
+#include <assert.h>
+
+
+std::string fileName(int arraySize, int arrayRange) {
+	std::string name = "number_";
+	std::string thousandsCount;
+	while (arraySize / 1000 > 0) {
+		thousandsCount += 'k';
+		arraySize /= 1000;
+	}
+
+	name += std::to_string(arraySize) + thousandsCount + '_' + std::to_string(arrayRange) + ".txt";
+	return name;
+}
+
+void vectorFromFile(const char* fileName, std::vector<int>& array) {
+	FILE* openedFile;
+	errno_t error_catch = fopen_s(&openedFile, fileName, "r");
+	assert(!error_catch);
+	while (!feof(openedFile)) {
+		int scannedNumber;
+		fscanf_s(openedFile, "%ld", &scannedNumber);
+		array.push_back(scannedNumber);
+	}
+	fclose(openedFile);
+}
+
+bool isSorted(std::vector<int> array) {
+	int size = array.size();
+	for (int i = 0; i < size - 1; i++) {
+		if (array[i] > array[i + 1])
+			return false;
+	}
+	return true;
+}
 
 int main() {
-	std::vector<int> array{ 9, 5, 1, 8, 0, 2 };
-	std::cout << HoarSort(array, 3) << std::endl; 
+	const int repeat_number = 3;
+	FILE* openedFile;
+	fopen_s(&openedFile, "report.txt", "w");
+	for (int arraySize = 10000; arraySize <= 1000000; arraySize *= 10) {
+		for (int arrayRange = 10; arrayRange <= 100000; arrayRange *= 100) {
+			double avgSum = 0;
+			for (int i = 0; i < repeat_number; i++) {
+				for (int type = 1; type <= 3; type++) {
+					std::vector <int> array;
+					vectorFromFile(fileName(arraySize, arrayRange).c_str(), array);
+					double end, start = clock();
+					HoarSort(array, type);
+					end = clock();
 
-	for (auto iter : array) {
-		std::cout << iter << ' ';	
+					double time = (end - start) / CLOCKS_PER_SEC;
+					avgSum += time;
+
+					FILE* openFile; // Печать сортированных файлов
+					const char* isSort = "sorted";
+					if (!isSorted(array)) isSort = "unsorted";
+					std::string sorted = isSort + fileName(arraySize, arrayRange);
+
+					fopen_s(&openFile, sorted.c_str(), "w");
+					for (auto iter : array) {
+						fprintf_s(openFile, "%ld ", iter);
+					}
+					fclose(openFile);
+
+
+					std::cout << "REP_NUM: " << i + 1 << " TYPE: "<< type << " SORTED: " << (isSorted(array) ? "true" : "false") << std::endl;
+					fprintf(openedFile, "REP_NUM: %d TYPE: %d SORTED : %d \n", i + 1, type, isSorted(array));
+				}
+			}
+			std::cout << std::endl << "SIZE: " << arraySize << " RANGE: " << arrayRange << " REP: " << repeat_number << " AVG_TIME: " << (double)(avgSum + 0.) / (repeat_number + 0.) << std::endl;
+			fprintf(openedFile, "SIZE:%d RANGE:%d REP:%d AVG_TIME:%f \n", arraySize, arrayRange, repeat_number, (double)(avgSum + 0.) / (repeat_number + 0.));
+			
+		}
 	}
+	fclose(openedFile);
 	return(0);
 }
