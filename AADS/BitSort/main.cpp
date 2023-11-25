a77ea1fe331c890e06d3dc5eb9865945555c0c8a
Helbushek <gorevaya134@gmail.com>
2023-11-25 12:27:28 +0700

    downloaded files with arrays

--- AADS/BitSort/main.cpp
+++ AADS/BitSort/main.cpp
@@ -38,52 +38,52 @@ bool isSorted(std::vector<int> array) {
 }
 
 int main() {
-	//const int repeat_number = 3;
-	//FILE* openedFile;
-	//fopen_s(&openedFile, "report.txt", "w");
-	//for (int arraySize = 10000; arraySize <= 1000000; arraySize *= 10) {
-	//	for (int arrayRange = 10; arrayRange <= 100000; arrayRange *= 100) {
-	//		double avgSum = 0;
-	//		for (int i = 0; i < repeat_number; i++) {
-	//			std::vector <int> array;
-	//			vectorFromFile(fileName(arraySize, arrayRange).c_str(), array);
-	//			double end, start = clock();
-	//			BitSort(array);
-	//			end = clock();
+	const int repeat_number = 3;
+	FILE* openedFile;
+	fopen_s(&openedFile, "report.txt", "w");
+	for (int arraySize = 10000; arraySize <= 1000000; arraySize *= 10) {
+		for (int arrayRange = 10; arrayRange <= 100000; arrayRange *= 100) {
+			double avgSum = 0;
+			for (int i = 0; i < repeat_number; i++) {
+				std::vector <int> array;
+				vectorFromFile(fileName(arraySize, arrayRange).c_str(), array);
+				double end, start = clock();
+				BitSort(array);
+				end = clock();
 
-	//			double time = (end - start) / CLOCKS_PER_SEC;
-	//			avgSum += time;
+				double time = (end - start) / CLOCKS_PER_SEC;
+				avgSum += time;
 
-	//			if (!isSorted) {
-	//				FILE* openFile; // Печать сортированных файлов
-	//				const char* isSort = "sorted";
-	//				if (!isSorted(array)) isSort = "unsorted";
-	//				std::string sorted = isSort + fileName(arraySize, arrayRange);
+				if (!isSorted) {
+					FILE* openFile; // Печать сортированных файлов
+					const char* isSort = "sorted";
+					if (!isSorted(array)) isSort = "unsorted";
+					std::string sorted = isSort + fileName(arraySize, arrayRange);
 
-	//				fopen_s(&openFile, sorted.c_str(), "w");
-	//				for (auto iter : array) {
-	//					fprintf_s(openFile, "%ld ", iter);
-	//				}
-	//				fclose(openFile);
-	//			}
+					fopen_s(&openFile, sorted.c_str(), "w");
+					for (auto iter : array) {
+						fprintf_s(openFile, "%ld ", iter);
+					}
+					fclose(openFile);
+				}
 
-	//			std::cout << "REP_NUM: " << i + 1 << " SORTED: " << (isSorted(array) ? "true" : "false") << std::endl;
-	//			fprintf(openedFile, "REP_NUM: %d SORTED : %d \n", i + 1, isSorted(array));
-	//		}
-	//		std::cout << std::endl << "SIZE: " << arraySize << " RANGE: " << arrayRange << " REP: " << repeat_number << " AVG_TIME: " << (double)(avgSum + 0.) / (repeat_number + 0.) << std::endl;
-	//		fprintf(openedFile, "SIZE:%d RANGE:%d REP:%d AVG_TIME:%f \n", arraySize, arrayRange, repeat_number, (double)(avgSum + 0.) / (repeat_number + 0.));
+				std::cout << "REP_NUM: " << i + 1 << " SORTED: " << (isSorted(array) ? "true" : "false") << std::endl;
+				fprintf(openedFile, "REP_NUM: %d SORTED : %d \n", i + 1, isSorted(array));
+			}
+			std::cout << std::endl << "SIZE: " << arraySize << " RANGE: " << arrayRange << " REP: " << repeat_number << " AVG_TIME: " << (double)(avgSum + 0.) / (repeat_number + 0.) << std::endl;
+			fprintf(openedFile, "SIZE:%d RANGE:%d REP:%d AVG_TIME:%f \n", arraySize, arrayRange, repeat_number, (double)(avgSum + 0.) / (repeat_number + 0.));
 
-	//	}
-	//}
-	//fclose(openedFile); 
+		}
+	}
+	fclose(openedFile); 
 
-	std::vector<int> array{ -1, -2, -3, 9, 8, 0 , 1, 2, 2, 2, 6, 10, 97};
+	/*std::vector<int> array{ -1, -2, -3, 9, 8, 0 , 1, 2, 2, 2, 6, 10, 97};
 
 	BitSort(array);
 
 	for (auto iter : array) {
 		std::cout << iter << ' ';
-	}
+	}*/
 
 	return(0);
 }
